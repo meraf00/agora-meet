9559d840ea315d8586ffa2eb58a44e255b35a419
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-07-22 16:43:32 +0300

    feat(agora-be): add websocket server

---
+++ agora-be/go.mod
@@ -0,0 +1,8 @@
+module github.com/meraf00/agora/agora-be
+
+go 1.22.1
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
---
+++ agora-be/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Type    string `json:"type"`
+	Payload string `json:"payload"`
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var meetings = make(map[string][]*websocket.Conn)
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	params := mux.Vars(r)
+	meetingId, ok := params["meetingId"]
+
+	if !ok {
+		fmt.Println("Invalid meeting id")
+		return
+	}
+
+	clients[conn] = true
+	meetings[meetingId] = append(meetings[meetingId], conn)
+
+	fmt.Println(clients)
+	fmt.Println(meetings)
+
+	for {
+		var msg Message
+
+		err := conn.ReadJSON(&msg)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, c := range meetings[meetingId] {
+			if c != conn {
+				err := c.WriteJSON(msg)
+
+				if err != nil {
+					fmt.Println(err)
+					delete(clients, c)
+				}
+			}
+		}
+
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/ws/{meetingId}", handleConnections)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "localhost:3000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	srv.ListenAndServe()
+}
