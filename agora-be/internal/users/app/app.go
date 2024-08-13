eb68964410378b7e9eba482ca73ac6e9eb2528c1
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-08-13 22:11:05 +0300

    feat(agora-be): add application

---
+++ agora-be/internal/users/app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"context"
+
+	"github.com/meraf00/agora-meet/agora-be/internal/users/app/query"
+	"github.com/meraf00/agora-meet/agora-be/internal/users/infrastructure"
+	"github.com/meraf00/agora-meet/agora-be/shared/config"
+)
+
+type Application struct {
+	Commands Commands
+	Queries  Queries
+}
+
+type Commands struct {
+}
+
+type Queries struct {
+	User query.UserHandler
+}
+
+func NewApplication(ctx context.Context) Application {
+
+	userRepo, err := infrastructure.NewUserMongoRepository(
+		config.AppConfig.Mongodb.Uri,
+		config.AppConfig.Mongodb.DbName,
+		"users",
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return Application{
+		Commands: Commands{},
+
+		Queries: Queries{
+			User: *query.NewUserHandler(userRepo),
+		},
+	}
+
+}
