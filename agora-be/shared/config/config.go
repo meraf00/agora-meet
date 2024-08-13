d8e9b915e07c6119b1763c4ab35e541d561af83e
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-08-13 22:01:34 +0300

    feat(agora-be): add app config

--- agora-be/shared/config/config.go
+++ agora-be/shared/config/config.go
@@ -1,7 +1,31 @@
 package config
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type mongodbConfig struct {
+	DbName string
+	Uri    string
+}
+
+var AppConfig = struct {
+	Port    int
+	Mongodb mongodbConfig
+}{}
 
 func LoadConfig() {
-	fmt.Println("Loading configurations")
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error loading config file %s", err)
+	}
+
+	AppConfig.Port = viper.GetInt("PORT")
+	AppConfig.Mongodb.Uri = viper.GetString("MONGODB_URI")
+	AppConfig.Mongodb.DbName = viper.GetString("MONGODB_DATABASE_NAME")
 }
